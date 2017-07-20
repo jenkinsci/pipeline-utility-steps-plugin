edae388722ea6143f3feb7174e78de75b1b3d2b2
Peter J. Kranz <Peter.Kranz@elektrobit.com>
2017-07-20 14:19:57 +0200

    JENKINS-43568: readProperties java.exe locks properties file
    adding try-with-resource blocks to FilePath#read calls

--- src/main/java/org/jenkinsci/plugins/pipeline/utility/steps/conf/ReadPropertiesStepExecution.java
+++ src/main/java/org/jenkinsci/plugins/pipeline/utility/steps/conf/ReadPropertiesStepExecution.java
@@ -31,6 +31,8 @@ import org.jenkinsci.plugins.workflow.steps.AbstractSynchronousNonBlockingStepEx
 import org.jenkinsci.plugins.workflow.steps.StepContextParameter;
 
 import javax.inject.Inject;
+
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.StringReader;
 import java.util.HashMap;
@@ -64,7 +66,9 @@ public class ReadPropertiesStepExecution extends AbstractSynchronousNonBlockingS
         if (!StringUtils.isBlank(step.getFile())) {
             FilePath f = ws.child(step.getFile());
             if (f.exists() && !f.isDirectory()) {
-                properties.load(f.read());
+                try (InputStream is = f.read()) {
+                    properties.load(is);
+                }
             } else if (f.isDirectory()) {
                 logger.print("warning: ");
                 logger.print(f.getRemote());
