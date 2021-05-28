d7a2bc65ca02e484df03b3298eab86818e8b3a76
offa <bm-dev@yandex.com>
2021-05-28 10:37:39 +0200

    sha1 step refactored, code moved to base class.

--- src/main/java/org/jenkinsci/plugins/pipeline/utility/steps/fs/FileSha1Step.java
+++ src/main/java/org/jenkinsci/plugins/pipeline/utility/steps/fs/FileSha1Step.java
@@ -25,24 +25,8 @@
 package org.jenkinsci.plugins.pipeline.utility.steps.fs;
 
 import hudson.Extension;
-import hudson.FilePath;
 import hudson.model.Descriptor;
-import hudson.remoting.VirtualChannel;
-import hudson.util.FormValidation;
-import jenkins.MasterToSlaveFileCallable;
-import org.apache.commons.lang.StringUtils;
-import org.jenkinsci.plugins.workflow.steps.*;
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
-
-import javax.annotation.Nonnull;
-import javax.inject.Inject;
-import java.io.*;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Collections;
-import java.util.Formatter;
-import java.util.Set;
 
 
 /**
@@ -50,109 +34,17 @@ import java.util.Set;
  *
  * @author Emanuele Zattin &lt;emanuelez@gmail.com&gt;.
  */
-public class FileSha1Step extends Step {
-    private final String file;
-
+public class FileSha1Step extends FileHashStep {
     @DataBoundConstructor
     public FileSha1Step(String file) throws Descriptor.FormException {
-        if (StringUtils.isBlank(file)) {
-            throw new Descriptor.FormException("can't be blank", "file");
-        }
-        this.file = file;
+        super(file, "SHA1");
     }
 
-    public String getFile() {
-        return file;
-    }
-
-    @Override
-    public StepExecution start(StepContext context) throws Exception {
-        return new ExecutionImpl(this, context);
-    }
 
     @Extension
-    public static class DescriptorImpl extends StepDescriptor {
-
+    public static class DescriptorImpl extends FileHashStep.DescriptorImpl {
         public DescriptorImpl() {
-
-        }
-
-        @Override
-        public Set<? extends Class<?>> getRequiredContext() {
-            return Collections.singleton(FilePath.class);
-        }
-
-        @Override
-        public String getFunctionName() {
-            return "sha1";
-        }
-
-        @Override
-        @Nonnull
-        public String getDisplayName() {
-            return "Compute the SHA1 of a given file";
-        }
-
-        @SuppressWarnings("unused")
-        public FormValidation doCheckFile(@QueryParameter String value) {
-            if (StringUtils.isBlank(value)) {
-                return FormValidation.error("Needs a value");
-            } else {
-                return FormValidation.ok();
-            }
+            super("SHA1");
         }
     }
-
-    public static class ExecutionImpl extends SynchronousNonBlockingStepExecution<String> {
-        private static final long serialVersionUID = 1L;
-
-        private transient FileSha1Step step;
-
-        protected ExecutionImpl(@Nonnull FileSha1Step step, @Nonnull StepContext context) {
-            super(context);
-            this.step = step;
-        }
-
-        @Override
-        protected String run() throws Exception {
-            FilePath ws = getContext().get(FilePath.class);
-            FilePath filePath = ws.child(step.getFile());
-            return filePath.act(new ComputeSha1());
-        }
-
-        private static class ComputeSha1 extends MasterToSlaveFileCallable<String> {
-            @Override
-            public String invoke(File file, VirtualChannel virtualChannel) throws IOException, InterruptedException {
-                if (file.exists() && file.isFile()) {
-                    try {
-                        return sha1(file);
-                    } catch (NoSuchAlgorithmException e) {
-                        throw new IOException(e.getMessage(), e);
-                    }
-                } else {
-                    return null;
-                }
-            }
-
-            public String sha1(final File file) throws NoSuchAlgorithmException, IOException {
-                final MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
-
-                try (InputStream is = new BufferedInputStream(new FileInputStream(file))) {
-                    final byte[] buffer = new byte[1024];
-                    for (int read = 0; (read = is.read(buffer)) != -1;) {
-                        messageDigest.update(buffer, 0, read);
-                    }
-                }
-
-                // Convert the byte to hex format
-                try (Formatter formatter = new Formatter()) {
-                    for (final byte b : messageDigest.digest()) {
-                        formatter.format("%02x", b);
-                    }
-                    return formatter.toString();
-                }
-            }
-        }
-    }
-
 }
