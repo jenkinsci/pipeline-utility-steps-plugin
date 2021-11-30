d0e1777f4252c8e4a742946ad61992fb000a22db
Basil Crow <me@basilcrow.com>
2021-11-29 22:21:22 -0800

    Remove unused import

--- src/main/java/org/jenkinsci/plugins/pipeline/utility/steps/conf/ReadPropertiesStepExecution.java
+++ src/main/java/org/jenkinsci/plugins/pipeline/utility/steps/conf/ReadPropertiesStepExecution.java
@@ -40,7 +40,6 @@ import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 
 /**
  * Execution of {@link ReadPropertiesStep}.
