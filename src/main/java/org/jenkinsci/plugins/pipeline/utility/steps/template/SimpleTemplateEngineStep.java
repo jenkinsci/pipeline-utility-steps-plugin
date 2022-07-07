bcbf85121346f411c5e4cd546aee291314380931
Martin d'Anjou <martin.danjou14@gmail.com>
2022-07-07 13:35:35 -0400

    Add SimpleTemplateEngineStep

---
+++ src/main/java/org/jenkinsci/plugins/pipeline/utility/steps/template/SimpleTemplateEngineStep.java
@@ -0,0 +1,89 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Nikolas Falco
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.jenkinsci.plugins.pipeline.utility.steps.template;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.Extension;
+import java.util.Map;
+import org.jenkinsci.plugins.pipeline.utility.steps.AbstractFileOrTextStep;
+import org.jenkinsci.plugins.pipeline.utility.steps.AbstractFileOrTextStepDescriptorImpl;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.StepExecution;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
+
+/**
+ * Binds a map to a groovy template using the SimpleTemplateEngine.
+ * Similar to the following code, but without the CPS issues.
+ * <code>
+ *  @NonCPS
+ *  String render(String template, Map binding) {
+ *      def engine = new groovy.text.SimpleTemplateEngine()
+ *      return engine.createTemplate(template).make(binding).toString()
+ *  }
+ * </code>
+ *
+ * @author Martin d'Anjou
+ */
+public class SimpleTemplateEngineStep extends AbstractFileOrTextStep {
+
+    protected Map<String, Object> bindings;
+
+    @DataBoundConstructor
+    public SimpleTemplateEngineStep() {
+    }
+
+    @Override
+    public StepExecution start(StepContext context) throws Exception {
+        return new SimpleTemplateEngineStepExecution(this, context);
+    }
+
+    @Extension
+    public static class DescriptorImpl extends AbstractFileOrTextStepDescriptorImpl {
+
+        public DescriptorImpl() {
+
+        }
+
+        @Override
+        public String getFunctionName() {
+            return "simpleTemplateEngine";
+        }
+
+        @Override
+        @NonNull
+        public String getDisplayName() {
+            return Messages.SimpleTemplateEngineStep_DescriptorImpl_displayName();
+        }
+    }
+
+    public Map<String, Object> getBindings() {
+        return bindings;
+    }
+
+    @DataBoundSetter
+    public void setBindings(Map<String, Object> bindings) {
+        this.bindings = bindings;
+    }
+}
---
+++ src/main/java/org/jenkinsci/plugins/pipeline/utility/steps/template/SimpleTemplateEngineStepExecution.java
@@ -0,0 +1,85 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Nikolas Falco
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.jenkinsci.plugins.pipeline.utility.steps.template;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.FilePath;
+import groovy.text.SimpleTemplateEngine;
+import groovy.text.Template;
+import org.apache.commons.io.IOUtils;
+import org.jenkinsci.plugins.pipeline.utility.steps.AbstractFileOrTextStepExecution;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.apache.commons.lang.StringUtils.isBlank;
+import static org.apache.commons.lang.StringUtils.isNotBlank;
+
+/**
+ * Execution of {@link SimpleTemplateEngineStep}.
+ *
+ * @author Martin d'Anjou
+ */
+public class SimpleTemplateEngineStepExecution extends AbstractFileOrTextStepExecution<Object> {
+    private static final long serialVersionUID = 1L;
+
+    private transient SimpleTemplateEngineStep step;
+
+    protected SimpleTemplateEngineStepExecution(@NonNull SimpleTemplateEngineStep step, @NonNull StepContext context) {
+        super(step, context);
+        this.step = step;
+    }
+
+    @Override
+    protected Object doRun() throws Exception {
+        String fName = step.getDescriptor().getFunctionName();
+        if (isNotBlank(step.getFile()) && isNotBlank(step.getText())) {
+            throw new IllegalArgumentException(Messages.SimpleTemplateEngineStepExecution_tooManyArguments(fName));
+        }
+
+        SimpleTemplateEngine engine = new SimpleTemplateEngine();
+        Template template = null;
+        if (!isBlank(step.getFile())) {
+            FilePath f = ws.child(step.getFile());
+            if (f.exists() && !f.isDirectory()) {
+                try (InputStream is = f.read()) {
+                    template = engine.createTemplate(IOUtils.toString(is, StandardCharsets.UTF_8));
+                }
+            } else if (f.isDirectory()) {
+                throw new IllegalArgumentException(Messages.SimpleTemplateEngineStepExecution_fileIsDirectory(f.getRemote()));
+            } else if (!f.exists()) {
+                throw new FileNotFoundException(Messages.SimpleTemplateEngineStepExecution_fileNotFound(f.getRemote()));
+            }
+        }
+        if (!isBlank(step.getText())) {
+            template = engine.createTemplate(step.getText().trim());
+        }
+
+        return template.make(step.getBindings());
+    }
+
+}
